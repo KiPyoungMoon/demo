0c180cba4474c317ced519ce50bc9410ea3e1de6
KPMoon <mkpong8202@gmail.com>
2022-10-26 13:42:20 +0900

    SPRING JPA DEMO FIRST COMMIT.

---
+++ src/main/java/spring/jpa/mvc/demo/App.java
@@ -0,0 +1,13 @@
+package spring.jpa.mvc.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
